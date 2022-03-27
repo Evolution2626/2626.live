8949b47c4e42e2f8ec3494f68dac83d7a3ec8287
Felix Giffard <technonerdzfg@gmail.com>
2022-03-27 10:32:33 -0400

    loop to update match infos

--- js/archivejs.js
+++ js/archivejs.js
@@ -3,78 +3,74 @@ hashparams = getHashParams();
 
 var team = hashparams["t"];
 var eventid = hashparams["e"];
-var matchnb = hashparams["m"];
+var matchkey = hashparams["m"];
 
-
-var matchesreq = new XMLHttpRequest();
 
 if (!team) team = 2626;
-if (!eventid) eventid = '2020qcsh';
-if (!matchnb) matchnb = 1;
 
 resize();
 
-matchnb = matchnb - 1;
-matchesreq.onreadystatechange = function() {
+var panelrequest = new XMLHttpRequest();
+panelrequest.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    matchesresp = JSON.parse(this.responseText);
+    teamsyp = JSON.parse(this.responseText);
 
-    var sortedmatches = Enumerable.From(matchesresp)
-    .Where(function (x) { return true})
-    .OrderBy(function (x) {
-      var addvalue = 0;
-      switch (x.comp_level) {
-        case "qm":
-          addvalue = 0;
-          break;
-        case "ef":
-          addvalue = 500;
-          break;
-        case "qf":
-          addvalue = 1000;
-          break;
-        case "sf":
-          addvalue = 1500;
-          break;
-        case "f":
-          addvalue = 2000;
-          break;
-        default:
-          addvalue = 0;
-      }
+    var lftmenu = document.getElementById('leftmenu');
+    for (var i = teamsyp.length-1; i >=0 ; i--) {
+      lftmenu.innerHTML += '<li class="mdl-menu__item" onclick="setHashParams([[\'e\', \'' + teamsyp[i]["key"] + '\']]); location.reload();">' + teamsyp[i]["key"] + '</li>';
+    }
 
-      return (x.match_number + addvalue + (x.set_number * 20));
-     })
-    .OrderBy(function(x) { return x.event_key })
-    .Select(function (x) { return x })
-    .ToArray();
+    if (!eventid) eventid = getLatestEvent(teamsyp)["key"];
 
-    matchesresp = sortedmatches;
-
-
-    showVideo(matchesresp[matchnb].videos[0].key, matchesresp[0].key, matchnb+1);
-
-    suggestVids(matchesresp);
-
+    getEventMatches();
   }
 };
-matchesreq.open('GET', 'https://www.thebluealliance.com/api/v3/team/frc' + team +'/event/' + eventid + '/matches?X-TBA-Auth-Key=wZjnIpA1EB2hq82k6hsmGHAGcsuqHJHrjLOeWp6MJTPuviWiUyipqLZsfa9kE3Ze');
-matchesreq.send();
+panelrequest.open('GET', 'https://www.thebluealliance.com/api/v3/team/frc' + team + '/events/simple?X-TBA-Auth-Key=wZjnIpA1EB2hq82k6hsmGHAGcsuqHJHrjLOeWp6MJTPuviWiUyipqLZsfa9kE3Ze');
+panelrequest.send();
 
-var panelrequest = new XMLHttpRequest();
-    panelrequest.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        teamsyp = JSON.parse(this.responseText);
+function getEventMatches() {
+  var matchesreq = new XMLHttpRequest();
+  matchesreq.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      matchesresp = JSON.parse(this.responseText);
+  
+      matchesresp.sort(function(a,b){return (new Date(a["time"]*1000) - new Date(b["time"]*1000))})
+  
+      if (!matchkey) matchkey = matchesresp[0].key;
+  
+  
+      showVideo(getMatchByKey(matchesresp, matchkey).videos[0].key, getMatchByKey(matchesresp, matchkey).key);
+  
+      suggestVids(matchesresp);
+  
+    }
+  };
+  matchesreq.open('GET', 'https://www.thebluealliance.com/api/v3/team/frc' + team +'/event/' + eventid + '/matches?X-TBA-Auth-Key=wZjnIpA1EB2hq82k6hsmGHAGcsuqHJHrjLOeWp6MJTPuviWiUyipqLZsfa9kE3Ze');
+  matchesreq.send();
+}
 
-        for (var i = teamsyp.length-1; i >=0 ; i--) {
-          var lftmenu = document.getElementById('leftmenu');
-          lftmenu.innerHTML += '<li class="mdl-menu__item" onclick="setHashParams([[\'e\', \'' + teamsyp[i] + '\']]); location.reload();">' + teamsyp[i] + '</li>';
-        }
+function getLatestEvent(events) {
+  events.sort(function(a,b) {return new Date(a["start_date"]) - new Date(b["start_date"])})
+  let latestEvent = null;
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
 
-      }
-    };
-    panelrequest.open('GET', 'https://www.thebluealliance.com/api/v3/team/frc' + team + '/events/keys?X-TBA-Auth-Key=nPMen3xyCoAZEXFnyhx0SFae6fLNmpyohlQb74J9BaHojEp0jCg8AE8iBur9w8cF');
-    panelrequest.send();
+    if (!latestEvent || new Date(event["start_date"]) < new Date()) {
+      latestEvent = event;
+    }
+  }
+  return latestEvent;
+}
+
+function getMatchByKey(matches, key){
+  for (let i = 0; i < matches.length; i++) {
+    const match = matches[i];
+    if (match["key"] == key) {
+      return match;
+    }
+  }
+  return null;
+}
 
 function suggestVids(matchesresp){
 
@@ -83,22 +79,21 @@ function suggestVids(matchesresp){
   for (var i = 0; i < matchesresp.length; i++) {
     if (typeof matchesresp[i].videos[0] !== 'undefined') {
     var suggestedmvid = matchesresp[i].videos[0].key;
-    var suggestedmid = i + 1;
     var sveventkey = matchesresp[i].event_key;
     var svtitle = matchesresp[i].key;
     var svrealtitle = svtitle.replace(sveventkey + '_', "");
     //suggestedvids.innerHTML += '<div class="mdl-cell mdl-cell--2-col"><p><a href="/archives?t=' + team + '&amp;e=' + eventid + '&amp;m=' + suggestedmid + '&amp;ap=1"><img src="https://i.ytimg.com/vi/' + suggestedmvid + '/hqdefault.jpg" alt="" width="100%" /></a></p><h4 style="text-align: center;">' + svrealtitle + '</h4></div>';
 
-    suggestedvids.innerHTML += '<div class="video-playlist-item" style="position: relative; margin-bottom: 5px;" onclick="showVideo(&quot;' + suggestedmvid + '&quot;, &quot;' + svtitle + '&quot;, &quot;' + suggestedmid + '&quot;);"> <div class="video-playlist-item-image" style="background-image: url(&quot;https://i.ytimg.com/vi/' + suggestedmvid + '/hqdefault.jpg&quot;); width: 100%; height: 168px; background-position: center; background-size: 100%;"></div> <div class="video-playlist-item-details" style="position: absolute; bottom: 10px; left: 10px; right: 10px; margin: 0; padding: 0;"> <span class="video-playlist-item-details-title" style="display: block; font-size: 16px; font-weight: bold; color: #fff; text-shadow: 1px 1px 3px #000;">' + svrealtitle + '</span> <span class="video-playlist-item-details-description"></span> </div> </div>';
+    suggestedvids.innerHTML += '<div class="video-playlist-item" style="position: relative; margin-bottom: 5px;" onclick="showVideo(&quot;' + suggestedmvid + '&quot;, &quot;' + svtitle + '&quot;);"> <div class="video-playlist-item-image" style="background-image: url(&quot;https://i.ytimg.com/vi/' + suggestedmvid + '/hqdefault.jpg&quot;); width: 100%; height: 168px; background-position: center; background-size: 100%;"></div> <div class="video-playlist-item-details" style="position: absolute; bottom: 10px; left: 10px; right: 10px; margin: 0; padding: 0;"> <span class="video-playlist-item-details-title" style="display: block; font-size: 16px; font-weight: bold; color: #fff; text-shadow: 1px 1px 3px #000;">' + svrealtitle + '</span> <span class="video-playlist-item-details-description"></span> </div> </div>';
     }
   }
 
 }
 
-function showVideo(videokey, matchkey, matchid) {
+function showVideo(videokey, matchkey) {
   var mainvid = document.getElementById('mainvid');
 
-  setHashParams([['m', matchid]]);
+  setHashParams([['m', matchkey]]);
 
   mainvid.innerHTML = '<iframe width="100%" height="700px" src="https://www.youtube.com/embed/' + videokey + '?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allowfullscreen id="ytplay"></iframe>';
   mainvid.innerHTML += '<p style="text-align: center;"><iframe width="100%" height="100px" src="https://frccards.com/match?m=' + matchkey + '" frameborder="0" scrolling="no" id="frccard"></iframe></p>';
